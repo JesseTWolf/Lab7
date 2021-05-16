05be4618d2f7e93a764e7fedb082add52aed1d6e
Jesse <jtwolf@ucsd.edu>
2021-05-16 13:30:41 -0700

    service worker added, most of the code adapted from https://developers.google.com/web/fundamentals/primers/service-workers

--- sw.js
+++ sw.js
@@ -4,3 +4,51 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+// Registration of the service worker is completed within the script.js file.
+
+// Setup the cache name and definte the urls that will be cached.
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  './index.html',
+  './style.css',
+  './scripts/router.js',
+  './scripts/script.js',
+  './components/entry-page.js',
+  './components/journal-entry.js',
+  'https://cse110lab6.herokuapp.com/entries'
+];
+
+// Event listener for installation
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Event listener for fetch requests
+self.addEventListener('fetch', function(event) {
+    console.log('Within fetch');
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+// Event listener for activation ( check out MDN's clients.claim() for this step )
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+  });
+  
